cc4cffff97bc470d7cbe3018d1d4fcdad767c8ec
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-18 17:50:03 +0100

    created jumbotron component

---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,24 @@
+<script>
+import { store } from '../store';
+
+export default {
+    name: 'Jumpotron',
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <img src="" alt="">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
