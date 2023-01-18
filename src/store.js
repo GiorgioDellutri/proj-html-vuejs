dce34aa740451e8df14354cc681d1ce43c67fa3c
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-18 23:45:59 +0100

    created array object in store.js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,38 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-
+    cardsPartnership: [
+        {
+            imgUrl: "/src/assets/svg/svg-0.svg",
+            title: 'Online Coaching',
+            text: 'Lorem ipsum is simply dummy text of the print ing and typesetting has been the industries'
+        },
+        {
+            imgUrl: "/src/assets/svg/svg-1.svg",
+            title: 'Consultation',
+            text: 'Lorem ipsum is simply dummy text of the print ing and typesetting has been the industries'
+        },
+        {
+            imgUrl: "/src/assets/svg/svg-2.svg",
+            title: 'Life Programs',
+            text: 'Lorem ipsum is simply dummy text of the print ing and typesetting has been the industries'
+        },
+    ],
+    prova: [
+        {
+            img: '/src/assets/img/course-1-2.jpg',
+            title: 'dasdasdas',
+            text: 'Lorem ipsum is simply dummy text of the print ing and typesetting has been the industries'
+        },
+        {
+            imgUrl: './assets/svg/svg-2.svg',
+            title: 'dasdasdas',
+            text: 'Lorem ipsum is simply dummy text of the print ing and typesetting has been the industries'
+        },
+        {
+            imgUrl: './assets/svg/svg-3.svg',
+            title: 'Life asdasd',
+            text: 'Lorem ipsum is simply dummy text of the print ing and typesetting has been the industries'
+        },
+    ]
 });
