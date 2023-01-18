eada095e428104838c5b3ce235699c9ac27b7338
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-18 23:45:36 +0100

    removed AppNavbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script >
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
-import AppNavbar from './components/AppNavbar.vue';
 
 export default {
   components: {
     AppHeader,
     AppMain,
-    AppNavbar,
   }
 }
 
