8b3bd118bc58fafda4859debdc08abf80c9579a8
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-18 23:48:45 +0100

    add jumbo context and style css class

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -2,7 +2,7 @@
 import { store } from '../store';
 
 export default {
-    name: 'Jumpotron',
+    name: 'Jumbotron',
     data() {
         return {
             store,
@@ -14,11 +14,42 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <img src="" alt="">
+    <div class="jumbotron-container  d-flex justify-content-center align-items-start">
+        <div class="jumbo-content">
+            <h1>Professional Development For <br> Coaches And Leaders</h1>
+            <div class="jumbo-button">
+                <h5>Learn more</h5>
+            </div>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables' as *;
 
+div.jumbotron-container {
+    height: 1000px;
+    background-image: url(../assets/img/jumbotron_1.PNG);
+    background-size: contain;
+    background-repeat: no-repeat;
+
+    div.jumbo-content {
+        padding: 0 7rem;
+        text-align: center;
+        margin-top: 9rem;
+
+        h1 {
+            font-size: 3.6rem;
+        }
+
+        div.jumbo-button {
+            margin: 0 auto;
+            border: 1px solid black;
+            margin-top: 4rem;
+            width: 200px;
+            padding: 1rem 0;
+        }
+    }
+}
 </style>
