9eb0846dfafabdcb2cbfa307c5760d6732bb6150
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-18 23:49:46 +0100

    changed entry in navbar & created for cicle for card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,15 @@
 <script>
 import { store } from '../store';
+import AppCards from '../components/AppCards.vue';
+
 
 export default {
     name: 'AppMain',
+
+    components: {
+        AppCards,
+    },
+
     data() {
         return {
             store,
@@ -14,9 +21,38 @@ export default {
 </script>
 
 <template>
-
+    <div class="container-fluid">
+        <div class="wrapper">
+            <p class="partners-title">Since 2000, weve been partnering with education leaders to <span>increase equity
+                    and achievement</span>
+                for all students.</p>
+            <div class="d-flex">
+                <AppCards v-for="(singlecard) in store.cardsPartnership" :card="singlecard" />
+            </div>
+        </div>
+    </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss" scoped >
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables' as *;
 
+
+div.container-fluid {
+    background-color: $white-smoke-color;
+
+    div.wrapper {
+        text-align: center;
+
+        p.partners-title {
+            padding: 0 6rem;
+            font-size: 2rem;
+            font-weight: bold;
+
+            span {
+                color: $red-text-color
+            }
+        }
+    }
+}
 </style>
