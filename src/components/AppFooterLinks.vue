3c6db8a15d1d71a5810749c96b8a0c63ed4299f3
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-20 17:12:22 +0100

    update content component

--- src/components/AppFooterLinks.vue
+++ src/components/AppFooterLinks.vue
@@ -12,16 +12,16 @@ export default {
 </script>
 
 <template>
-    <div class="container">
+    <div class="container ">
         <h5>{{ card.title }}</h5>
         <ul>
             <li v-if="card.address"><i class="fa-solid fa-map-pin"></i>{{ card.address }}</li>
-            <li v-if="card.phone"><i class="fa-solid fa-square-phone"></i>{{ card.phone }}</li>
-            <li v-if="card.email"><i class="fa-solid fa-envelope"></i>{{ card.email }}</li>
-            <li v-if="card.link_1"><i class="fa-solid fa-chevron-right"></i>{{ card.link_1 }}</li>
-            <li v-if="card.link_2"><i class="fa-solid fa-chevron-right"></i>{{ card.link_2 }}</li>
-            <li v-if="card.link_3"><i class="fa-solid fa-chevron-right"></i>{{ card.link_3 }}</li>
-            <li v-if="card.link_4"><i class="fa-solid fa-chevron-right"></i>{{ card.link_4 }}</li>
+            <li v-if="card.phone"><a href="#"><i class="fa-solid fa-square-phone"></i>{{ card.phone }}</a></li>
+            <li v-if="card.email"><a href="#"><i class="fa-solid fa-envelope"></i>{{ card.email }}</a></li>
+            <li v-if="card.link_1"><a href="#"><i class="fa-solid fa-chevron-right"></i>{{ card.link_1 }}</a></li>
+            <li v-if="card.link_2"><a href="#"><i class="fa-solid fa-chevron-right"></i>{{ card.link_2 }}</a></li>
+            <li v-if="card.link_3"><a href="#"><i class="fa-solid fa-chevron-right"></i>{{ card.link_3 }}</a></li>
+            <li v-if="card.link_4"><a href="#"><i class="fa-solid fa-chevron-right"></i>{{ card.link_4 }}</a></li>
         </ul>
     </div>
 </template>
@@ -42,8 +42,16 @@ div.container {
         margin: .8rem 0 .8rem 0;
         color: $light-blue-gray-text-color;
 
-        i {
-            padding: .3rem;
+        a {
+            color: $light-blue-gray-text-color;
+
+            &:hover {
+                color: $dark-red-text-color;
+            }
+
+            i {
+                padding: .3rem;
+            }
         }
     }
 }
