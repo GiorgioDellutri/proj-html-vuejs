29adb075ccb7853feda25c40f0e2929ca3f54bfe
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-20 13:01:13 +0100

    content AppFooterLinks

--- src/components/AppFooterLinks.vue
+++ src/components/AppFooterLinks.vue
@@ -14,6 +14,15 @@ export default {
 <template>
     <div class="container">
         <h5>{{ card.title }}</h5>
+        <ul>
+            <li v-if="card.address"><i class="fa-solid fa-map-pin"></i>{{ card.address }}</li>
+            <li v-if="card.phone"><i class="fa-solid fa-square-phone"></i>{{ card.phone }}</li>
+            <li v-if="card.email"><i class="fa-solid fa-envelope"></i>{{ card.email }}</li>
+            <li v-if="card.link_1"><i class="fa-solid fa-chevron-right"></i>{{ card.link_1 }}</li>
+            <li v-if="card.link_2"><i class="fa-solid fa-chevron-right"></i>{{ card.link_2 }}</li>
+            <li v-if="card.link_3"><i class="fa-solid fa-chevron-right"></i>{{ card.link_3 }}</li>
+            <li v-if="card.link_4"><i class="fa-solid fa-chevron-right"></i>{{ card.link_4 }}</li>
+        </ul>
     </div>
 </template>
 
@@ -22,10 +31,20 @@ export default {
 @use '../styles/partials/variables' as *;
 
 div.container {
-    padding: 3rem;
+    padding: 2.5rem;
+
 
     h5 {
         font-weight: bold;
+    }
+
+    ul li {
+        margin: .8rem 0 .8rem 0;
+        color: $light-blue-gray-text-color;
+
+        i {
+            padding: .3rem;
+        }
     }
 }
 </style>
