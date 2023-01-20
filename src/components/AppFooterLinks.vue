f7553ed89ca197ac2a91cd7aeeef996132d2e616
GiorgioDellutri <giorgiodll93@gmail.com>
2023-01-20 11:52:26 +0100

    created AppFooterLinks content

---
+++ src/components/AppFooterLinks.vue
@@ -0,0 +1,31 @@
+
+<script>
+export default {
+    name: 'AppFooterLinks',
+
+    props: {
+        card: {
+
+        }
+    },
+}
+</script>
+
+<template>
+    <div class="container">
+        <h5>{{ card.title }}</h5>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables' as *;
+
+div.container {
+    padding: 3rem;
+
+    h5 {
+        font-weight: bold;
+    }
+}
+</style>
