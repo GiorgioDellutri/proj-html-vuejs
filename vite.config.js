8db8434616847c0b9ce80817ca37ccf641bdc3d9
GiorgioDellutri <giorgiodellutridev@gmail.com>
2025-02-04 09:13:26 +0100

    Added deploy scripts to package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'proj-html-vuejs', // Sostituisci con il nome esatto del tuo repo GitHub
   plugins: [vue()],
 })
