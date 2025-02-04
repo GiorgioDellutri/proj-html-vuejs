0d834f9bf6f9df4bcf303c9c142f56cd68a9728b
GiorgioDellutri <giorgiodellutridev@gmail.com>
2025-02-04 09:14:37 +0100

    Updates

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  base: 'proj-html-vuejs', // Sostituisci con il nome esatto del tuo repo GitHub
-  plugins: [vue()],
-})
